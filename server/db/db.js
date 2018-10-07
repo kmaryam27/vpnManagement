57f46d6364486ebb865a5b6dc4e1bc9948719ffa
Rick Terry <fdterr@gmail.com>
2018-10-07 00:19:22 -0400

    creds

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,8 @@ const pkg = require('../../package.json');
 const databaseName = 'vpnmanagement';
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL ||
+    `postgres://postgres:rickisgreat@localhost:5432/${databaseName}`,
   {
     logging: false,
   }

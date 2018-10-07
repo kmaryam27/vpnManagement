d71f314cd7682b5baba6c19ea6fb99d20b1cc550
Rick Terry <fdterr@gmail.com>
2018-10-07 00:18:23 -0400

    sockets?

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,10 @@
 const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 
-const databaseName = vpnmanagement;
+const databaseName = 'vpnmanagement';
 
 const db = new Sequelize(
-  process.env.DATABASE_URL ||
-    `postgres://postgres:rickisgreat@localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
   }

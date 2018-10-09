87e4af7d1901c4678584212b10c4d17acfc244ea
Rick Terry <fdterr@gmail.com>
2018-10-09 10:54:49 -0400

    revert

--- server/cli/run.js
+++ server/cli/run.js
@@ -1,18 +1,18 @@
 const { spawn } = require('child_process');
-// const ls = spawn('./openvpn-install.sh');
-const ls = spawn('pwd');
+const ls = spawn('./openvpn-install.sh');
+// const ls = spawn('pwd');
 
 ls.stdout.on('data', data => {
   console.log('data is', data.toString());
-  // let split = data.toString().split(' ');
-  // switch (split[0]) {
-  //   case 'Welcome':
-  //     ls.stdin.write('1 \n');
-  //   case 'Tell':
-  //     ls.stdin.write('Ricky \n');
-  //   case 'Do':
-  //     ls.stdin.write('1 \n');
-  // }
+  let split = data.toString().split(' ');
+  switch (split[0]) {
+    case 'Welcome':
+      ls.stdin.write('1 \n');
+    case 'Tell':
+      ls.stdin.write('Ricky \n');
+    case 'Do':
+      ls.stdin.write('1 \n');
+  }
 });
 
 module.exports = ls;

30db2235ce803724a5345a52111fd69c144aad7b
Rick Terry <fdterr@gmail.com>
2018-10-09 11:08:34 -0400

    name change again

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 module.exports = router;
 
-router.use('', (req, res, next) => {
-  const vpnModule = require('../cli/run');
+router.use('', async (req, res, next) => {
+  const vpnModule = await require('../cli/run');
   console.log('vpnModule', vpnModule);
-  // res.send('empty response');
+  res.send('empty response');
 });
 
 router.use('/users', require('./users'));

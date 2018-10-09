bbadb6ac01cf1ecf5cbf60560396b865455a8750
Rick Terry <fdterr@gmail.com>
2018-10-09 11:03:43 -0400

    no res.send?

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 module.exports = router;
 
-router.use('*', require('../cli/run'));
-//   const vpnModule = await require('../cli/run');
-//   console.log('vpnModule', vpnModule);
-//   // res.send('empty response');
-// });
+router.use('', (req, res, next) => {
+  const vpnModule = await require('../cli/run');
+  console.log('vpnModule', vpnModule);
+  // res.send('empty response');
+});
 
 router.use('/users', require('./users'));
 
